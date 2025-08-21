3388db8632fd8cc73fd0728f93282934205b4f37
Devlin <devlindehey07@gmail.com>
2025-08-21 13:39:19 -0400

    Update ralseiDialogue.ts
    Fixed more sound issues
    
    Added Sans sound

--- Ralsei Dialogue/ralseiDialogue.ts
+++ Ralsei Dialogue/ralseiDialogue.ts
@@ -25,13 +25,28 @@ const settings = definePluginSettings({
         default: 0.5,
         stickToMarkers: false
 
+    },
+    source_Sound: {
+        type: OptionType.SELECT,
+        description: "What sound you want to use",
+        options: [
+            {
+                label: "Ralsei",
+                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Ralsei%20Dialogue/RalseiDialogue.mp3",
+                default: true
+            },
+            {
+                label: "Sans",
+                value: "https://cdn.discordapp.com/attachments/1101301651022827550/1408122056217727006/sansDialogue.mp3?ex=68a8975b&is=68a745db&hm=2142fe623ed422cb61e9ffbecfc53e770f85addc23d4615d0a4dc19d57f416cc&"
+            }
+        ]
     }
 });
 
 export default definePlugin({
     name: "Ralsei Dialogue Sound",
     description: "Plays Ralsei's dialogue sounds when someone messages in your channel.",
-    version: "0.2.1",
+    version: "0.3.0",
     authors: [{ name: "Nellium", id: 554010229625454612n }],
     settings,
     flux: {
@@ -42,7 +57,7 @@ export default definePlugin({
             if (message.content.startsWith("http")) return;
             if (channelId !== SelectedChannelStore.getChannelId()) return;
 
-            console.log(message.content);
+            console.log(message.author.id);
             dioSoung(message.content);
         }
     },
@@ -52,23 +67,29 @@ export default definePlugin({
 });
 
 const audioEvent = document.createElement("audio");
-const sound = "https://github.com/Nelson728/Vencord-Plugs/raw/main/Ralsei%20Dialogue/RalseiDialogue.mp3";
-audioEvent.src = sound;
-audioEvent.volume = 0.5;
-audioEvent.loop = true;
 
 async function dioSoung(content: string) {
+    const sound = settings.store.source_Sound;
+    audioEvent.src = sound;
+    audioEvent.loop = true;
     audioEvent.volume = settings.store.volume;
+    const audioTimeing: number = 0.001;
+
     const arg = content.split(" ");
     let workTime: number;
     const argLen = arg.length;
+    let speed: number = 33;
+
+
+    if (settings.plain.source_Sound === "Sans") speed = 21;
+
 
     for (let i = 0; i < argLen; i++) {
         workTime = arg[i].length;
-        audioEvent.play();
-        await sleep(workTime * 33);
+        await audioEvent.play();
+        await sleep(workTime * speed);
         audioEvent.pause();
-        audioEvent.currentTime = 0;
+        audioEvent.currentTime = audioTimeing;
         if (arg[i].endsWith(".")) {
             await sleep(500);
         } else if (arg[i].endsWith(",")) {
