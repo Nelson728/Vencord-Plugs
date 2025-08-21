723dd035c87ce1e365e2ee6b4c85d2fa30e4ab01
Devlin <devlindehey07@gmail.com>
2025-08-21 13:52:09 -0400

    Update ralseiDialogue.ts
    Hotfix
    
    Ralsei did not work for some reason so i changed it back to discord

--- Ralsei Dialogue/ralseiDialogue.ts
+++ Ralsei Dialogue/ralseiDialogue.ts
@@ -32,7 +32,7 @@ const settings = definePluginSettings({
         options: [
             {
                 label: "Ralsei",
-                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Ralsei%20Dialogue/RalseiDialogue.mp3",
+                value: "https://cdn.discordapp.com/attachments/1088254993276092516/1408067156926529676/RalseiDialogue.mp3?ex=68a8643a&is=68a712ba&hm=a7cff4e1f13d16aa08cdadfb2c23237893928dcaa58bb47d281c2f9ac2fbf52c&",
                 default: true
             },
             {
@@ -46,7 +46,7 @@ const settings = definePluginSettings({
 export default definePlugin({
     name: "Ralsei Dialogue Sound",
     description: "Plays Ralsei's dialogue sounds when someone messages in your channel.",
-    version: "0.3.0",
+    version: "0.3.1",
     authors: [{ name: "Nellium", id: 554010229625454612n }],
     settings,
     flux: {
