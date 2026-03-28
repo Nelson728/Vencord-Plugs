1e40ee9b98e365043a41419738d7b2508a43c217
Devlin <devlindehey07@gmail.com>
2026-03-28 01:10:56 +0000

    Updated Dialogue plugin
    Fixed issue #1
    Provided constant links for sounds

--- Undertale Dialogue/index.ts
+++ Undertale Dialogue/index.ts
@@ -10,7 +10,6 @@ import definePlugin, { OptionType } from "@utils/types";
 import { Message } from "@vencord/discord-types";
 import { SelectedChannelStore } from "@webpack/common";
 
-
 interface IMessageCreate {
     type: "MESSAGE_CREATE";
     optimistic: boolean;
@@ -27,45 +26,45 @@ const settings = definePluginSettings({
         stickToMarkers: false
 
     },
-    source_Sound: {
+    sourceSound: {
         type: OptionType.SELECT,
         description: "What sound you want to use",
         options: [
             {
                 label: "Ralsei",
-                value: "https://cdn.discordapp.com/attachments/1088254993276092516/1408067156926529676/RalseiDialogue.mp3?ex=68a8643a&is=68a712ba&hm=a7cff4e1f13d16aa08cdadfb2c23237893928dcaa58bb47d281c2f9ac2fbf52c&",
+                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Sounds/ralsei-talk.mp3",
                 default: true
             },
             {
                 label: "Sans",
-                value: "https://cdn.discordapp.com/attachments/1101301651022827550/1408122056217727006/sansDialogue.mp3?ex=68a8975b&is=68a745db&hm=2142fe623ed422cb61e9ffbecfc53e770f85addc23d4615d0a4dc19d57f416cc&"
+                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Sounds/sansDialogue.mp3"
             },
             {
                 label: "Noelle",
-                value: "https://cdn.discordapp.com/attachments/1088254993276092516/1408151025906356316/noelle.mp3?ex=68a8b256&is=68a760d6&hm=31496862d67b883d0d19383fd9589c2a374ff5a965cb1456432fc8b9374d9690&"
+                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Sounds/noelle.mp3"
             },
             {
                 label: "Spamton",
-                value: "https://cdn.discordapp.com/attachments/1088254993276092516/1408178895907192992/Spamton.mp3?ex=68a8cc4b&is=68a77acb&hm=6c123aa671e4ff8a9eb41f598282dfebc10a23ee7f65b85579fe68115699b6e2&"
+                value: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Sounds/Spamton.mp3"
             }
         ]
     },
-    custom_Source_Toggle: {
+    customSourceToggle: {
         type: OptionType.BOOLEAN,
         description: "Enable the custom sound source *(note: link must be from discord and must be a mp3)",
         default: false
     },
-    custom_Source: {
+    customSource: {
         type: OptionType.STRING,
-        description: "Source sound *(note: link must be from discord and must be a mp3)",
-        default: "https://cdn.discordapp.com/attachments/1101301651022827550/1408122056217727006/sansDialogue.mp3?ex=68a8975b&is=68a745db&hm=2142fe623ed422cb61e9ffbecfc53e770f85addc23d4615d0a4dc19d57f416cc&",
+        description: "Source sound *(note: link must be from Discord/raw Github and must be a mp3)",
+        default: "https://github.com/Nelson728/Vencord-Plugs/raw/main/Sounds/Coins.mp3",
         restartNeeded: true
     },
-    custom_Speed: {
+    customSpeed: {
         type: OptionType.NUMBER,
         description: "How long each character is *(in ms)",
         default: 21
-    }
+    },
 });
 
 export default definePlugin({
@@ -78,15 +77,14 @@ export default definePlugin({
         async MESSAGE_CREATE({ type, optimistic, message, channelId }: IMessageCreate) {
             // const myId = UserStore.getCurrentUser().id;
             if (optimistic || type !== "MESSAGE_CREATE" || message.state === "SENDING" || !message.content || message.content.includes("https://") || channelId !== SelectedChannelStore.getChannelId()) return;
-            console.log(message.author.id);
             playDialogueSound(message.content);
         }
     },
     start() {
         console.log("Ralsei online!!");
-        if (settings.plain.custom_Source_Toggle && (!settings.plain.custom_Source.includes(".mp3") || !settings.plain.custom_Source.startsWith("https://cdn.discordapp.com/attachments/"))) {
+        if (settings.plain.customSourceToggle && (!settings.plain.customSource.includes(".mp3") || !settings.plain.customSource.startsWith("https://cdn.discordapp.com/attachments/"))) {
             alert("Invalid Sound");
-            settings.plain.custom_Source_Toggle = false;
+            settings.plain.customSourceToggle = false;
         }
     }
 });
@@ -97,21 +95,19 @@ async function playDialogueSound(content: string) {
     let workTime: number;
     let speed: number;
 
-    let sound = settings.store.source_Sound;
-    if (settings.plain.custom_Source_Toggle) sound = settings.plain.custom_Source;
+    let sound = settings.store.sourceSound;
+    if (settings.plain.customSourceToggle) sound = settings.plain.customSource;
     audioEvent.src = sound;
     audioEvent.loop = true;
     audioEvent.volume = settings.store.volume;
 
-
-
     // Speed check
 
-    if (!settings.plain.custom_Source_Toggle) {
-        if (settings.plain.source_Sound.includes("RalseiDialogue.mp3")) speed = 33;
-        else if (settings.plain.source_Sound.includes("Spamton.mp3")) speed = 50;
+    if (!settings.plain.customSourceToggle) {
+        if (settings.plain.sourceSound.includes("RalseiDialogue.mp3")) speed = 33;
+        else if (settings.plain.sourceSound.includes("Spamton.mp3")) speed = 50;
         else speed = 21;
-    } else speed = settings.plain.custom_Speed;
+    } else speed = settings.plain.customSpeed;
 
     // Sound handler
 
